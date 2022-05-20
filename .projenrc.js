a80204ddf7d27cc6d861720692173890d7d24ce0
Ari Palo <ari.palo@almamedia.fi>
2022-05-21 02:03:36 +0300

    feat: update projen configuration and upgrade dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -9,28 +9,51 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorName: 'Alma Media',
   authorOrganization: true,
   authorAddress: 'opensource@almamedia.dev',
-  name: '@almamedia-open-source/cdk-cross-region-parameter',
-  repositoryUrl: 'https://github.com/almamedia-open-source/cdk-project-target.git',
-  keywords: ['cdk', 'aws-cdk', 'awscdk', 'aws'],
+  name: '@alma-cdk/cross-region-parameter',
+  description: 'Combine the power of AWS CDK & OpenAPI YAML Schema Definitions ',
+  repositoryUrl: 'https://github.com/alma-cdk/cross-region-parameter.git',
+  keywords: ['cdk', 'aws-cdk', 'awscdk', 'aws', 'cloudfront', 'api-gateway', 'rest-api', 'open-api', 'swagger'],
 
   // Publish configuration
   defaultReleaseBranch: 'main',
+  packageManager: javascript.NodePackageManager.NPM,
   npmAccess: javascript.NpmAccess.PUBLIC,
 
   // Dependencies
   minNodeVersion: nodejsVersion,
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.24.1',
   constructsVersion: '10.0.0',
-  peerDeps: ['constructs', 'aws-cdk-lib'],
-  devDeps: ['constructs', 'aws-cdk-lib', '@types/change-case'],
-  bundledDeps: ['change-case'],
+  peerDeps: [
+    'constructs',
+    'aws-cdk-lib',
+  ],
+  devDeps: [
+    'constructs',
+    'aws-cdk-lib',
+    '@types/prettier@v2.6.0',
+    '@types/change-case',
+  ],
+  bundledDeps: [
+    'change-case',
+  ],
 
   // Gitignore
-  gitignore: ['.DS_Store'],
+  gitignore: [
+    '.DS_Store',
+    '/examples/**/cdk.context.json',
+    '/examples/**/node_modules',
+    '/examples/**/cdk.out',
+    '/examples/**/.git',
+  ],
+
+
 });
 
 new TextFile(project, '.nvmrc', {
   lines: [nodejsVersion],
 });
+
+project.addPackageIgnore('/examples/');
+
 
 project.synth();
